7bf0252277b7fceeb6a0e530d191ba20c0e4ae24
habib-gulomov <gulomovhabibulloh@gmail.com>
2022-08-07 21:43:43 +0500

    add all

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [value,setvalue] = useState('')
+  const [data, setData] = useState([])
+  const [save, setSave] = useState([])
+  // console.log(data);
+  // console.log(value);
+
+  useEffect(()=>{
+   save.push(value)
+  },[value])
+
+  useEffect(()=>{
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${value}&appid=6241352dd86a4183c3f7840c04e41c00&units=metric`)
+    .then((res) =>res.json())
+    .then((data) => setData(data))
+  },[value])
+
+//  console.log(data);
+  // let newsave = save.splice(save.length -5,1);
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+      <input onKeyUp={((e) => e.key == 'Enter' ? setvalue(e.target.value) : 'error' )} type="text" />
+    <div>
+    {
+    data.length && data.map((e)=>{
+      
+     return(
+      
+      <div>
+        <p>{e.name}</p>
+      </div>
+     )
+    })
+    }
+    </div>
+    <ul>
+      {
+       save.lenth && save.splice(save.length -5 ,5).map((e)=>(
+          <li>
+            <button>❤</button>
+            <button>{e}</button>
+          </li>
+        ))
+      }
+    </ul>
     </div>
   );
 }
